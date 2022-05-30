bc30bdd561216c99b82f7106236e11645cbae802
Koffi-Sk <kofdzakou1411@gmail.com>
2022-05-30 08:13:49 -0400

    Mise en place de mon routeur

---
+++ src/pages/About.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        exercitationem tempore id eos sed molestiae similique, in autem nam!
+        Iure, blanditiis id? Illo est nihil aliquid facilis distinctio aperiam,
+        voluptatem dolorem ex odio, nam expedita temporibus consequuntur
+        accusantium consequatur non fugit eligendi similique reiciendis!
+        Aspernatur quo cupiditate soluta reprehenderit iste architecto eligendi
+        illum deserunt perspiciatis sed voluptates aut, amet itaque recusandae
+        possimus ducimus, quod tenetur impedit velit iure veritatis. Maxime,
+        illo! Delectus, magnam impedit maiores vel ad rerum, nostrum ea
+        asperiores commodi temporibus eaque aliquid illo maxime ex aspernatur
+        cumque ipsum facilis, nemo qui voluptatum! Ipsa odit excepturi quibusdam
+        nisi.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias ab
+        et, esse numquam iste fugiat mollitia iusto tenetur assumenda nostrum
+        accusantium quia, repellat dicta, minima provident reprehenderit
+        explicabo exercitationem totam id ratione. Distinctio assumenda animi,
+        rem magni vero explicabo porro ipsam dignissimos necessitatibus, sint
+        reprehenderit quis! Accusantium quia quasi, unde non eos cupiditate
+        culpa suscipit? Odio ex quibusdam est quisquam inventore magnam
+        recusandae soluta eligendi maiores quaerat nam, delectus excepturi
+        eaque, provident placeat? Aliquam, veritatis suscipit ab velit inventore
+        nisi amet eveniet tempore recusandae nostrum repudiandae earum aut. Quis
+        laboriosam placeat similique aperiam sequi doloribus suscipit aliquid,
+        vero, dicta omnis reiciendis. Rem eligendi, quae illum magni itaque ex
+        eum eius illo doloremque aliquid quia voluptas culpa voluptatem,
+        explicabo consequatur molestias amet dignissimos expedita nihil aut
+        perferendis soluta tempore. Exercitationem minima eaque soluta suscipit
+        magni laborum quia doloribus aperiam odio hic ipsa accusantium eos,
+        officia quaerat quae aspernatur quo est omnis rem. Commodi, vero.
+        Sapiente, laborum adipisci dignissimos debitis voluptates suscipit,
+        aperiam molestias atque ipsa pariatur minima excepturi facere eaque
+        veritatis nobis reiciendis nulla laboriosam, perferendis perspiciatis
+        modi omnis quas doloribus asperiores. Nostrum ratione repellat
+        consectetur, dicta debitis blanditiis eum! Voluptatibus amet iste quam,
+        soluta in libero eaque aspernatur dolorem veniam pariatur autem error,
+        explicabo nobis maiores. Assumenda sequi reprehenderit minima voluptatum
+        ullam est rerum voluptate neque error blanditiis. Dolorem quasi ullam
+        laboriosam totam nostrum voluptates ipsam cupiditate animi impedit
+        cumque ab culpa, odio atque facilis perspiciatis! Aspernatur
+        necessitatibus sequi labore id expedita nam repellat beatae. Optio
+        beatae voluptas repudiandae velit eaque, cum aperiam hic quisquam
+        quibusdam. Assumenda rerum accusamus ipsum facere porro natus
+        consectetur mollitia reiciendis similique, non corporis repudiandae
+        asperiores quaerat sapiente vel illo quibusdam impedit est facilis sint?
+        Eveniet veritatis beatae tenetur eius repudiandae? Repudiandae sapiente
+        fugit minus ex suscipit, labore ab consequuntur fugiat temporibus unde
+        molestias illum!
+      </p>
+    </div>
+  );
+};
+
+export default About;
