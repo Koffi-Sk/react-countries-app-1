db6d1df21d95a5103c330a455d68427a85e3294a
Koffi-Sk <kofdzakou1411@gmail.com>
2022-05-30 16:05:45 -0400

    Création du formulaire du blog et connexion avec le serveur json

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,17 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
+  const [content, setContent] = useState("");
+  const [error, setError] = useState(false);
+
+  const getData = () => {
+    axios
+      .get("http://localhost:3001/articles")
+      .then((response) => console.log(response));
+  };
+  useEffect(() => getData(), []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (content.length < 140) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
   return (
     <div className="blog-container">
       <Logo />
       <Navigation />
       <h1>Blog</h1>
 
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input type="text" placeholder="Nom" />
-        <textarea placeholder="Message"></textarea>
+        <textarea
+          style={{ border: error ? "1px solid red" : "1px solid #61dafb" }} // Style conditionnel
+          placeholder="Message"
+          onChangeCapture={(e) => setContent(e.target.value)}
+        ></textarea>
+        {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
       <ul></ul>
