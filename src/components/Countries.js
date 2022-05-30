c00eef7e1aa2756c62edfbbfe36d4bb581b3a23a
Koffi-Sk <kofdzakou1411@gmail.com>
2022-05-30 09:43:35 -0400

    Composant Card et affichage des info de l'api par axios

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+  // Le useEffect se joue lorsque le composant est monté (appelé)
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
   return (
     <div className="countries">
-      <h1>COUNTRIES</h1>
+      <ul>
+        {data.map((country, index) => (
+          //<li key={index}>{country.translations.fra.common}</li>
+          <Card key={index} country={country} />
+        ))}
+      </ul>
     </div>
   );
 };
